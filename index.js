ea2fd583c66be6864e5787cbc58c5da626b7ebd3
Jessica Nelson <yahwehschica@yahoo.com>
2021-09-22 13:44:12 -0400

    Assignment Complete

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Your code here
+document.addEventListener("keydown", function(event) {
+    if (event.key === "ArrowLeft") {
+        const leftNumbers = dodger.style.left.replace("px", "");
+        const left = parseInt(leftNumbers, 10);
+        dodger.style.left = `${left - 1}px`;
+            }
+});
+
+const dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+    if (left > 0) {
+        dodger.style.left = `${left - 1}px`;
+    }
+}
+
+function moveDodgerRight() {
+    const rightNumbers = dodger.style.left.replace("px", "");
+    const right = parseInt(rightNumbers, 10);
+    if (right < 180) {
+        dodger.style.left = `${right + 1}px`;
+    }
+}
